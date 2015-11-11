72cfe23714f820fb05eaff0a90c5ddf0b04bb836
ErinEMcC <erinm35@gmail.com>
2015-11-11 13:40:04 -0500

    Adding the WHILE loop
    To scrape next article : author, date, title, JSTOR sources, paragraph
    text

--- jstor_daily.py
+++ jstor_daily.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 #Ex. indv. article, practice use. Eventually want to scrape all articles
-url = 'http://daily.jstor.org/visual-literacy-in-the-age-of-open-content/'
+url = 'http://daily.jstor.org/deforestation-lead-drought/'
 
 a_article = requests.get (url)
 
@@ -20,7 +20,9 @@ art_author = soup.find('span', attrs = {"class": "entry-author"}).text
 art_date = soup.find("span", attrs = {"class": "entry-date"}).text
 art_title = soup.find("h1", attrs = {"class": "entry-title single-title"}).text
 
-print ('\n')
+print (art_author)
+print (art_date)
+print (art_title)
 
 #create variable for JSTOR citation (article acting as source)
 art_source = soup.find_all("h3", attrs = {"class": "citation-title"})
@@ -29,25 +31,53 @@ for link in art_source:
 
     print (a_link.get('href'))
 
-print ('\n')
-print (art_author, art_date, art_title)
-print ('\n')
-
 #create a variable for the actual text of an article (could be one or many paragraphs)
 art_frame = soup.find_all ("div", attrs = {"class": "single-box clearfix entry-content"})
 for text in art_frame:
     art_text = text.find_all("p")
 
     print (art_text)
+
     print ('\n')
 
 #finding the "Next" link
 n_link = soup.find("a",attrs = {"rel": "next"})
 print (n_link.get('href'))
 
-#while n_link == true
-#url2 = n_link.get('href')
-#b_article = requests.get(url2)
+while n_link != None:
+    url = n_link['href']
+    a_article = requests.get(url)
+    soup = BeautifulSoup(a_article.text, "html.parser")
+    art_author = soup.find('span', attrs = {"class": "entry-author"}).text
+    art_date = soup.find("span", attrs = {"class": "entry-date"}).text
+    art_title = soup.find("h1", attrs = {"class": "entry-title single-title"}).text
+
+    print (art_author)
+    print (art_date)
+    print (art_title)
+
+    #create variable for JSTOR citation (article acting as source)
+    art_source = soup.find_all("h3", attrs = {"class": "citation-title"})
+    for link in art_source:
+        a_link = link.find("a")
+
+        print ("JSTOR: ", a_link.get('href'))
+
+    #create a variable for the actual text of an article (could be one or many paragraphs)
+    art_frame = soup.find_all ("div", attrs = {"class": "single-box clearfix entry-content"})
+    for text in art_frame:
+        art_text = text.find_all("p")
+
+        print (art_text)
+
+        print ('\n')
+
+    n_link = soup.find("a",attrs = {"rel": "next"})
+    print (n_link)
+    
+    print ('\n')
+    
+
 
 
 
