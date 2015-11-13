be48740f93cf9ef3a12c71d00536160efd5ec210
ErinEMcC <erinm35@gmail.com>
2015-11-13 18:44:35 -0500

    Successful code to pull metadata from JSTOR_Daily articles
    DOIs extracted from URLs
    lists made for dictionary keys: "Journals" and "Publishers"

--- jstorDaily_noContent.py
+++ jstorDaily_noContent.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import time
+import re
+
 
 #list of dictionaries - each dictionary one article
 jdaily_articles = []
 
 #Ex. indv. article, practice use. Eventually want to scrape all articles
-url = 'http://daily.jstor.org/deforestation-lead-drought/'
+#start with oldest article: http://daily.jstor.org/maya-angelou-has-died/
+url = 'http://daily.jstor.org/maya-angelou-has-died'
 
 a_article = requests.get (url)
 if a_article.status_code != 200:
@@ -24,18 +29,32 @@ if 'By' in art_author2:
 
 #creating other variables for article metadata
 art_date = soup.find("span", attrs = {"class": "entry-date"}).text
-art_title = soup.find("h1", attrs = {"class": "entry-title single-title"}).text
+
+title = soup.find("h1", attrs = {"class": "entry-title single-title"}).text
+if '\n' in title:
+    art_title = title.replace ('\n' , ''). strip()
+
 
 #create list for JSTOR citation(s) / article(s) acting as source
-jstor_links = []
+jstor_dois = []
 
-#create variable for indv. Jstor citations
+#create variable for indv. Jstor citations - just the DOI suffix
 art_source = soup.find_all("h3", attrs = {"class": "citation-title"})
 for link in art_source:
     a_link = link.find("a")
     #add link(s) to jstor_links list
-    jstor_links.append(a_link.get('href'))
-    #replace('http://www.jstor.org/stable/' , '')
+    text = a_link.get('href')
+    doi = text.replace('http://www.jstor.org/stable/' , '')
+    jstor_dois.append(doi)
+
+#create list for titles of journals used as sources
+journals_sourced = []
+
+#Journal Name, Vol/Issue information
+for title_source in soup.find_all('p', attrs = {"class": "citation-source"}):
+    a_source = title_source.text
+    #append journals to list
+    journals_sourced.append(a_source)
 
 #create list for and article's 'tag' variables
 jdaily_tags = []
@@ -56,19 +75,22 @@ this_article = {}
 
 #assigning data fields as dictionary keys, with the variables created above
 this_article['date'] = art_date
-this_article['author'] = art_author
-this_article['title'] = art_title
-this_article['sources'] = jstor_links
+this_article['jdaily_author'] = art_author
+this_article['jdaily_title'] = art_title
+this_article['sources'] = jstor_dois
 this_article['tags'] = jdaily_tags
+this_article['journals'] = journals_sourced
+this_article ['publishers'] = "None listed"
 
 #add the dictionary to the list created at top or return message
 jdaily_articles.append(this_article)
 
-
+print (this_article)
 
 while n_link != None:
     url = n_link['href']
     a_article = requests.get(url)
+    time.sleep(1)
     soup = BeautifulSoup(a_article.text, "html.parser")
 
     #creating, cleaning author variable
@@ -78,18 +100,46 @@ while n_link != None:
 
     #creating other variables for article metadata
     art_date = soup.find("span", attrs = {"class": "entry-date"}).text
-    art_title = soup.find("h1", attrs = {"class": "entry-title single-title"}).text
+    
+    title = soup.find("h1", attrs = {"class": "entry-title single-title"}).text
+    if '\n' in title:
+        art_title = title.replace ('\n' , ''). strip()
 
     #create list for JSTOR citation(s) / article(s) acting as source
-    jstor_links = []
+    jstor_dois = []
 
-    #create variable for indv. Jstor citations
+    #create variable for indv. Jstor citations - just the DOI suffix
     art_source = soup.find_all("h3", attrs = {"class": "citation-title"})
     for link in art_source:
         a_link = link.find("a")
-
         #add link(s) to jstor_links list
-        jstor_links.append(a_link.get('href'))
+        text = a_link.get('href')
+        doi = text.replace('http://www.jstor.org/stable/' , '')
+        jstor_dois.append(doi)
+                
+    #add doi(s) to jstor_dois list
+    jstor_dois.append(doi)
+        
+    #create list for titles of journals used as sources
+    journals_sourced = []
+    
+    #Journal Name, Vol/Issue information
+    for title_source in soup.find_all('p', attrs = {"class": "citation-source"}):
+        a_source = title_source.text
+        #append journals to list
+        journals_sourced.append(a_source)
+
+       
+    #create list for publishers of journals used as sources
+    publishers_sourced = []
+    
+    #Publisher Statement
+    for publisher_source in soup.find_all('p', attrs = {"class": "citation-source"}):
+        a_pub = publisher_source.text
+
+        #append journals to list
+        publishers_sourced.append(a_pub)
+
 
     #create list for and article's 'tag' variables
     jdaily_tags = []
@@ -98,8 +148,8 @@ while n_link != None:
     for tag in soup.find_all("a", attrs = {"rel": "tag"}):
         a_tag = tag.string
 
-    #append tag(s) to list
-    jdaily_tags.append(a_tag)
+        #append tag(s) to list
+        jdaily_tags.append(a_tag)
     
     #find > create variable for next page link
     n_link = soup.find("a",attrs = {"rel": "next"})
@@ -109,17 +159,21 @@ while n_link != None:
 
     #assigning data fields as dictionary keys, with the variables created above
     this_article['date'] = art_date
-    this_article['author'] = art_author
-    this_article['title'] = art_title
-    this_article['sources'] = jstor_links
+    this_article['jdaily_author'] = art_author
+    this_article['jdaily_title'] = art_title
+    this_article['sources'] = jstor_dois
     this_article['tags'] = jdaily_tags
+    this_article['journals'] = journals_sourced
+    this_article['publishers'] = publishers_sourced
 
     #add the dictionary to the list created at top or return message
     jdaily_articles.append(this_article)
 
-print(jdaily_articles)
+    print(this_article)
+    print("\n")
 
-
+with open('scraped_jdaily.json', 'w') as f:
+    f.write(json.dumps(jdaily_articles, indent=4))
 
 
 
